a51c6ffa3e5cb64dd2a3bb0ec62064e2577847ba
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-03-23 21:34:49 +0200

    step1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+let all =[];
 $('document').ready(function () {
 
   const Gallery = function(name) {
@@ -9,37 +9,58 @@ $('document').ready(function () {
     this.description = name.description;
     this.keyword = name.keyword;
     this.horns = name.horns;
+    all.push(this);
   };
 
-  Gallery.prototype.render = function() {
-    let galleryContainer = $('.photoTemplate').clone();
-    $('main').append(galleryContainer);
-    let list = $('<option></option>').text(this.keyword);
-    $('select').append(list);
-    galleryContainer.find('h2').text(this.title);
-    galleryContainer.find('img').attr('src', this.image_url);
-    galleryContainer.find('p').text(this.description);
-    galleryContainer.find('.keyword').text(this.keyword);
-    galleryContainer.find('article').text(`Horns Number Is: ${this.horns}`);
-    galleryContainer.attr('class', this.keyword);
+  // Gallery.prototype.render = function() {
+  //   let galleryContainer = $('.photoTemplate').clone();
+  //   $('main').append(galleryContainer);
+  //   let list = $('<option></option>').text(this.keyword);
+  //   $('select').append(list);
+  //   galleryContainer.find('h2').text(this.title);
+  //   galleryContainer.find('img').attr('src', this.image_url);
+  //   galleryContainer.find('p').text(this.description);
+  //   galleryContainer.find('.keyword').text(this.keyword);
+  //   galleryContainer.find('article').text(`Horns Number Is: ${this.horns}`);
+  //   galleryContainer.attr('class', this.keyword);
+
+  // };
+  Gallery.prototype.renderTwo = function() {
+    let template = $('#mustache-template').html();
+    console.log(template);
+    //2. use Mustache to render new html .render method
+    let html = Mustache.render(template, this);
+    //3.return the html
+    return html;
 
   };
-
   const ajaxSettings = {
     method: 'get',
     dataType: 'json'
   };
 
-  $.ajax('data/page-1.json', ajaxSettings).then(data => {
+  // $.ajax('data/page-1.json', ajaxSettings).then(data => {
+  //   data.forEach(item => {
+  //     let buildup = new Gallery(item);
+  //     buildup.render();
+  //   });
+  // });
+
+  $.ajax('data/page-2.json', ajaxSettings).then(data => {
     data.forEach(item => {
       let buildup = new Gallery(item);
-      buildup.render();
+      buildup.renderTwo();
     });
-  });
-});
+  }); });
 
-$('select').on('change', function () {
-  let selected = this.value;
-  $('section').hide();
-  $(`.${selected}`).show();
-});
+
+// $('select').on('change', function () {
+//   let selected = this.value;
+//   if(this.value !== 'default') {
+//     $('section').hide();
+//     $(`.${selected}`).show();
+//   } else {
+//     $('section').show();
+//   }
+
+// });
